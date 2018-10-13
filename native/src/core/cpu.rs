a6420e9352a49c58befd507ecba52b9c63d81275
nukeop <nukeop@gumblert.tech>
2018-10-13 02:07:28 +0200

    Implement some cpu instructions

--- native/src/core/cpu.rs
+++ native/src/core/cpu.rs
@@ -1,3 +1,5 @@
+use core::memory::{RAM,Memory};
+
 pub struct Registers {
     x: u8,
     y: u8,
@@ -17,17 +19,54 @@ impl Registers {
 }
 
 pub struct CPU {
-    regs: Registers
+    pub regs: Registers,
+    pub ram: RAM
 }
 
 impl CPU {
     pub fn new() -> CPU {
         CPU {
-            regs: Registers::new()
+            regs: Registers::new(),
+            ram: RAM::new()
         }
     }
 
     pub fn step(&mut self) {
-        
+        let next = self.load_byte_increment_pc();
+        self.decode(next);
+    }
+
+    pub fn load_byte_increment_pc(&mut self) -> u8 {
+        let pc = self.regs.pc;
+        let val = self.ram.load_byte(pc);
+        self.regs.pc += 1;
+        val
+    }
+
+    pub fn decode(&mut self, opcode: u8) {
+        match opcode {
+            0x00 => self.noop(),
+            0x01 => self.mvix(),
+            _ => panic!("Unimplemented opcode: {:X}", opcode)
+        };
+    }
+
+    pub fn noop(&mut self) {}
+    pub fn mvix(&mut self) {
+        let pc = self.regs.pc;
+        let val = self.load_byte_increment_pc();
+        self.regs.x = val;
+    }
+
+    pub fn mviy(&mut self) {
+        let pc = self.regs.pc;
+        let val = self.load_byte_increment_pc();
+        self.regs.y = val;
+    }
+
+    pub fn mvit(&mut self) {
+        let pc = self.regs.pc;
+        let val = self.load_byte_increment_pc();
+        self.regs.t = val;
     }
 }
--- native/src/core/zeus.rs
+++ native/src/core/zeus.rs
@@ -3,22 +3,20 @@ use core::cpu::CPU;
 use core::memory::RAM;
 
 pub struct Zeus {
-    cpu: CPU,
-    pub ram: RAM
+    pub cpu: CPU
 }
 
 impl Zeus {
     pub fn new() -> Zeus {
         Zeus {
-            cpu: CPU::new(),
-            ram: RAM::new()
+            cpu: CPU::new()
         }
     }
 
     pub fn get_memory<'a>(&mut self, mut cx: FunctionContext<'a>) -> JsResult<'a, JsArray> {
         let arr = cx.empty_array();
 
-        for (i, byte) in self.ram.mem.iter().enumerate() {
+        for (i, byte) in self.cpu.ram.mem.iter().enumerate() {
             let num = cx.number(*byte as u32);
             arr.set(&mut cx, i as u32, num);
         }
